055f3100d069a1625dd0f12d91ad81cd0daa8a40
Mohit Soni <developer_mohitsoni@outlook.com>
2025-07-13 12:00:17 +0530

    refactor(SendEmailJobs): enhance message queue handling and logging and add a message Worker that process the queue messages

--- server/src/jobs/SendEmailJobs.ts
+++ server/src/jobs/SendEmailJobs.ts
@@ -2,8 +2,22 @@ import { Queue, Worker } from "bullmq";
 import { redis } from "../services/redisService.js";
 import { defaultQueueConfig } from "../config/queue.js";
 
-export const messageQueue = "gemini-message-queue";
-export const emailQueue = new Queue(messageQueue, {
+export const messageQueueName = "gemini-message-queue";
+export const messageQueue = new Queue(messageQueueName, {
   connection: redis,
   defaultJobOptions: defaultQueueConfig
 })
+
+export const handler = new Worker(messageQueueName, async (job)=>{
+  console.log(`Processing job ${job.id} in ${messageQueueName}`);
+}, {
+  connection: redis,
+  
+})
+
+handler.on("completed", (job) => {
+	console.log(`The job ${job.id} is completed.`); //
+});
+handler.on("failed", (job) => {
+	console.log(`The job ${job?.id} is failed.`);
+});
