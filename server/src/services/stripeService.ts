90f56078213c1d1b3224bd2722f85c57a1584e59
Mohit Soni <developer_mohitsoni@outlook.com>
2025-07-13 14:18:33 +0530

    feat(stripe): export stripe instance for external usage

--- server/src/services/stripeService.ts
+++ server/src/services/stripeService.ts
@@ -1,7 +1,7 @@
 import prisma from "../DB/db.config.js";
 import Stripe from 'stripe';
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || '', {
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || '', {
   apiVersion: "2025-06-30.basil",
 });
 
